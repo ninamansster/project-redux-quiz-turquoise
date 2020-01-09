0700900422513f2c94516b0af0f547cf875bfb60
Jenny Jacobsson <enni@oans.net>
2020-01-09 21:46:42 +0100

    small change

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { quiz } from 'reducers/quiz'
@@ -17,9 +16,6 @@ export const Answers = ({ question }) => {
     return null
   })
 
-  // // Alt. solution
-  // const [answerClicked, setAnswerClicked] = useState(-1)
-
   return (
     <div className="answer-wrap">
       {question && question.options.map((item, index) => {
@@ -29,7 +25,7 @@ export const Answers = ({ question }) => {
         if (checkAnswer !== null) {
           if (checkAnswer.answerIndex === index) {
           // Check if current option is same as saved answer index
-          // Add class based on correctness :O (right, wrong)
+          // Add class based on right or wrong answer.
             btnClasses.push(`${checkAnswer.isCorrect ? 'right' : 'wrong'}Answer`)
           } else if (!checkAnswer.isCorrect && question.correctAnswerIndex === index) {
           // When answer is wrong, check if current option is same as saved answer
@@ -38,22 +34,11 @@ export const Answers = ({ question }) => {
           }
         }
 
-        // // Alt. solution (non-Redux)
-        // // if (answerClicked > -1) {
-        // //   if (answerClicked === index) {
-        // //     btnClasses.push(`${index === question.correctAnswerIndex ? 'right' : 'wrong'}Answer`)
-        // //   } else if (index === question.correctAnswerIndex) {
-        // //     btnClasses.push('rightAnswer')
-        // //   }
-        // }
-
         return (
           <button
             className={btnClasses.join(' ')}
             type="button"
             onClick={() => {
-              // setAnswerClicked(index)
-
               dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
             }}>
             {item}
