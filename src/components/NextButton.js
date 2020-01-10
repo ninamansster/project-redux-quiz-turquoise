b58fd76c1c8723f333313ad9cd5be4699409d174
Linda <isell.linda@gmail.com>
2020-01-10 11:12:05 +0100

    added next button, fixed logic, show when answred

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { quiz } from 'reducers/quiz'
 import './button.css'
 
 export const NextButton = () => {
   const dispatch = useDispatch()
+  const answers = useSelector((state) => state.quiz.answers)
+  const currentQuestion = useSelector((state) => state.quiz.currentQuestionIndex)
+
+  console.log(answers.length)
+  console.log(currentQuestion)
 
   return (
     <>
-      <button type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
-        Next Question
-      </button>
+      {answers.length > currentQuestion &&
+        <button type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+          Next Question
+    </button>
+      }
     </>
   )
 }
