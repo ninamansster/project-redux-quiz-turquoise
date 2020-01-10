84091541fb067aa6390975dcadf5d81147a20f3e
Disa de Beau <Disan@Disas-MacBook-Air.local>
2020-01-10 15:08:05 +0100

    styling of current question

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { ProgressBarContainer } from './ProgressBar'
 import { Answers } from 'components/Answers'
 import { NextButton } from 'components/NextButton'
+import './currentQuestion.css';
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
@@ -12,12 +13,21 @@ export const CurrentQuestion = () => {
   }
 
   return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-      <Answers question={question} />
-      <NextButton />
-      <ProgressBarContainer />
+    <div className="currentQuestion">
+      <div className="currentQuestionHeader">
+        <h1>Question: {question.questionText}</h1>
+      </div>
+      <div className="currentQuestionAnswer">
+        <Answers question={question} />
+      </div>
+      <div className="currentQuestionNextButton">
+        <NextButton />
+      </div>
+      <div className="currentQuestionProgressBarWrapper">
+        <ProgressBarContainer />
+      </div>
     </div>
+
   )
 }
 
